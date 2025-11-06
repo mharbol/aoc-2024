ae85dc759b8b426adb9559a45e9d06ed49cd2633
Marcus Harbol <mharbol@users.noreply.github.com>
2025-11-05 19:22:48 -0700

    working with TEST_F

--- test/include/solutiontest.h
+++ test/include/solutiontest.h
@@ -2,16 +2,44 @@
 #ifndef __AOC_SOLUTIONTEST_H__
 #define __AOC_SOLUTIONTEST_H__
 
+#include "solution/Solution.h"
+#include "util/fileread.h"
+
 #include <cstddef>
+#include <gtest/gtest.h>
+#include <iomanip>
+#include <sstream>
 #include <string>
-namespace aoc {
-/**
- * Simple test function to verify solutions for each day so far with unit tests.
- * @param day the day to test
- * @param part1 expected solution for part 1
- * @param part2 expected solution for part 2
- */
-void testSolution(const size_t day, const std::string &part1, const std::string &part2);
-} // namespace aoc
+
+class DayTestYear : public ::testing::Test {
+
+private:
+    const std::string base_path = "../../resources/day";
+
+protected:
+    /**
+     * Simple test function to verify solutions for each day so far with unit tests.
+     * @param day the day to test
+     * @param part1 expected solution for part 1
+     * @param part2 expected solution for part 2
+     */
+    void testSolution(const size_t day, const std::string &part1, const std::string &part2) {
+
+        std::ostringstream input_file{};
+        input_file << base_path << std::setw(2) << std::setfill('0') << day << ".txt";
+
+        const auto solution = aoc::Solution::getDay(day);
+
+        const std::vector<std::string> lines = aoc::readInput(input_file.str());
+
+        const std::string p1_actual = solution->part1(lines);
+        const std::string p2_actual = solution->part2(lines);
+
+        ASSERT_EQ(part1, p1_actual)
+            << "Expected " << part1 << " for day " << day << " part 1 but got " << p1_actual;
+        ASSERT_EQ(part2, p2_actual)
+            << "Expected " << part2 << " for day " << day << " part 2 but got " << p2_actual;
+    }
+};
 
 #endif
--- test/test_days.cpp
+++ test/test_days.cpp
@@ -2,78 +2,80 @@
 #include "solutiontest.h"
 #include <gtest/gtest.h>
 
-TEST(TestDay, Day01) {
-    aoc::testSolution(1, "1579939", "20351745");
+using TestDay = DayTestYear;
+
+TEST_F(TestDay, Day01) {
+    testSolution(1, "1579939", "20351745");
 }
-TEST(TestDay, Day02) {
-    aoc::testSolution(2, "686", "717");
+TEST_F(TestDay, Day02) {
+    testSolution(2, "686", "717");
 }
-TEST(TestDay, Day03) {
-    aoc::testSolution(3, "159833790", "89349241");
+TEST_F(TestDay, Day03) {
+    testSolution(3, "159833790", "89349241");
 }
-TEST(TestDay, Day04) {
-    aoc::testSolution(4, "2390", "1809");
+TEST_F(TestDay, Day04) {
+    testSolution(4, "2390", "1809");
 }
-TEST(TestDay, Day05) {
-    aoc::testSolution(5, "6612", "4944");
+TEST_F(TestDay, Day05) {
+    testSolution(5, "6612", "4944");
 }
-TEST(TestDay, Day06) {
-    aoc::testSolution(6, "4778", "1618");
+TEST_F(TestDay, Day06) {
+    testSolution(6, "4778", "1618");
 }
-TEST(TestDay, Day07) {
-    aoc::testSolution(7, "12839601725877", "149956401519484");
+TEST_F(TestDay, Day07) {
+    testSolution(7, "12839601725877", "149956401519484");
 }
-TEST(TestDay, Day08) {
-    aoc::testSolution(8, "256", "1005");
+TEST_F(TestDay, Day08) {
+    testSolution(8, "256", "1005");
 }
-TEST(TestDay, Day09) {
-    aoc::testSolution(9, "6607511583593", "6636608781232");
+TEST_F(TestDay, Day09) {
+    testSolution(9, "6607511583593", "6636608781232");
 }
-TEST(TestDay, Day10) {
-    aoc::testSolution(10, "517", "1116");
+TEST_F(TestDay, Day10) {
+    testSolution(10, "517", "1116");
 }
-TEST(TestDay, Day11) {
-    aoc::testSolution(11, "233875", "277444936413293");
+TEST_F(TestDay, Day11) {
+    testSolution(11, "233875", "277444936413293");
 }
-TEST(TestDay, Day12) {
-    aoc::testSolution(12, "1433460", "855082");
+TEST_F(TestDay, Day12) {
+    testSolution(12, "1433460", "855082");
 }
-TEST(TestDay, Day13) {
-    aoc::testSolution(13, "34393", "83551068361379");
+TEST_F(TestDay, Day13) {
+    testSolution(13, "34393", "83551068361379");
 }
-TEST(TestDay, Day14) {
-    aoc::testSolution(14, "236628054", "7584");
+TEST_F(TestDay, Day14) {
+    testSolution(14, "236628054", "7584");
 }
-TEST(TestDay, Day15) {
-    aoc::testSolution(15, "1479679", "1509780");
+TEST_F(TestDay, Day15) {
+    testSolution(15, "1479679", "1509780");
 }
-TEST(TestDay, Day16) {
-    aoc::testSolution(16, "73432", "496");
+TEST_F(TestDay, Day16) {
+    testSolution(16, "73432", "496");
 }
-TEST(TestDay, Day17) {
-    aoc::testSolution(17, "2,0,7,3,0,3,1,3,7", "247839539763386");
+TEST_F(TestDay, Day17) {
+    testSolution(17, "2,0,7,3,0,3,1,3,7", "247839539763386");
 }
-TEST(TestDay, Day18) {
-    aoc::testSolution(18, "288", "52,5");
+TEST_F(TestDay, Day18) {
+    testSolution(18, "288", "52,5");
 }
-TEST(TestDay, Day19) {
-    aoc::testSolution(19, "233", "691316989225259");
+TEST_F(TestDay, Day19) {
+    testSolution(19, "233", "691316989225259");
 }
-TEST(TestDay, Day20) {
-    aoc::testSolution(20, "1307", "986545");
+TEST_F(TestDay, Day20) {
+    testSolution(20, "1307", "986545");
 }
-TEST(TestDay, Day21) {
-    aoc::testSolution(21, "94426", "118392478819140");
+TEST_F(TestDay, Day21) {
+    testSolution(21, "94426", "118392478819140");
 }
-TEST(TestDay, Day22) {
-    aoc::testSolution(22, "15006633487", "1710");
+TEST_F(TestDay, Day22) {
+    testSolution(22, "15006633487", "1710");
 }
-TEST(TestDay, Day23) {
-    aoc::testSolution(23, "1000", "cf,ct,cv,cz,fi,lq,my,pa,sl,tt,vw,wz,yd");
+TEST_F(TestDay, Day23) {
+    testSolution(23, "1000", "cf,ct,cv,cz,fi,lq,my,pa,sl,tt,vw,wz,yd");
 }
-TEST(TestDay, Day24) {
-    aoc::testSolution(24, "48063513640678", "hqh,mmk,pvb,qdq,vkq,z11,z24,z38");
+TEST_F(TestDay, Day24) {
+    testSolution(24, "48063513640678", "hqh,mmk,pvb,qdq,vkq,z11,z24,z38");
 }
-TEST(TestDay, Day25) {
-    aoc::testSolution(25, "2900", "");
+TEST_F(TestDay, Day25) {
+    testSolution(25, "2900", "");
 }
